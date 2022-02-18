9078cd078d6e87792a3d8fe411bc2acfb3f6fc6b
mhaecal <haecal.dk@gmail.com>
2022-02-18 21:28:17 +0700

    redux toolkit with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { addUser, selectUsers } from './features/users/usersSlice'
 
-interface User {
-  id: number
-  name: string
+export default function App() {
+	const dispatch = useAppDispatch()
+	const users = useAppSelector(selectUsers)
+
+	const [name, setName] = useState<string>('')
+
+	return (
+		<div>
+			<p>{JSON.stringify(users)}</p>
+			<input
+				type="text"
+				placeholder="Name"
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<button onClick={() => dispatch(addUser({ id: users.length + 1, name }))}>
+				Add User
+			</button>
+		</div>
+	)
 }
-
-const App: React.FC = () => {
-  // string | number | object | boolean | null | undefined
-  const [user, setUser] = useState<User>({
-    id: 1,
-    name: 'haecal',
-  })
-  const inputRef = useRef<HTMLInputElement>(null)
-  const divRef = useRef<HTMLInputElement>(null)
-
-  function handleInput() {
-    console.log(inputRef.current)
-  }
-
-  return (
-    <div ref={divRef}>
-      <h1>
-        {user.id} - {user.name}
-      </h1>
-      <input type="text" ref={inputRef} onChange={handleInput} />
-    </div>
-  )
-}
-
-export default App
