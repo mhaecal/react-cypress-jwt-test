fea957db16c019c83f1941cd8240d67f1bb7ba9f
mhaecal <haecal.dk@gmail.com>
2022-02-20 18:52:00 +0700

    react rooter 6 ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { useAppDispatch, useAppSelector } from './app/hooks'
-import { addUser, selectUsers } from './features/users/usersSlice'
+import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function App() {
-	const dispatch = useAppDispatch()
-	const users = useAppSelector(selectUsers)
-
-	const [name, setName] = useState<string>('')
-
-	return (
-		<div>
-			<p>{JSON.stringify(users)}</p>
-			<input
-				type="text"
-				placeholder="Name"
-				onChange={(e) => setName(e.target.value)}
-			/>
-			<button onClick={() => dispatch(addUser({ id: users.length + 1, name }))}>
-				Add User
-			</button>
-		</div>
-	)
+	return <Outlet />
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import App from './App'
+import MainLayout from './components/layouts/MainLayout'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import ProductItem from './pages/ProductItem'
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        {/*start routes*/}
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/product" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductItem />} />
+          </Route>
+          <Route element={<App />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+        {/*end routes*/}
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
