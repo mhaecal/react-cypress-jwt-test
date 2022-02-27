68aa55ac0e17fd103772e3ae197e255d2782db67
mhaecal <haecal.dk@gmail.com>
2022-02-27 16:42:08 +0700

    fix: router structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,32 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-import App from './App'
-import MainLayout from './components/layouts/MainLayout'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import ProductList from './pages/ProductList'
-import ProductItem from './pages/ProductItem'
+import Router from './router'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        {/*start routes*/}
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/product" element={<ProductList />} />
-            <Route path="/product/:id" element={<ProductItem />} />
-          </Route>
-          <Route element={<App />}>
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-        {/*end routes*/}
-      </BrowserRouter>
+      <Router />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
